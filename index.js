d52de35b20b7bc186e9383f13486d5453b6f84d4
Theo Mathieu <tmathieu.github@fastmail.com>
2023-06-06 17:41:54 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const sanitize = (val) => {
+    if (!val) {
+      return;
+    }
+    const sanitized = val.toLowerCase().replace(/\//g, '-').replace(/\./g, '-').replace(/[^a-z0-9\n-]/gi, '');
+    const properLength = sanitized.substring(0, 42)
+    const withoutDash = properLength[properLength.length-1] === "-" ?
+      properLength.substring(0, properLength.length-1) :
+      properLength;
+    return withoutDash;
+  };
+  
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const input = core.getInput('input');
+  console.log(`Input is ${input}!`);
+  const output = sanitize(input);
+  core.setOutput("ouput", output);
+  console.log(`The output: ${output}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
